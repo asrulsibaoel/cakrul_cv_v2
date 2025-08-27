b68d1a9b5a947075c444debc2ec699e59e44ef17
Asrul Sani Ariesandy <asrulsibaoel@gmail.com>
2025-08-28 02:54:03 +0700

    Fixed: Vite hosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     base: '/',
     plugins: [vue()],
+    server: {
+        host: '0.0.0.0',          // allow external access
+        port: 5173,               // keep internal container port
+        allowedHosts: ['cakrul.my.id', 'localhost', '127.0.0.1']  // add your domain here
+    },
     css: {
         preprocessorOptions: {
             scss: {
